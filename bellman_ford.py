54cd475150765252e1185af0520df9306c39a61e
ehdrbs <zlzlakdhkd@gmail.com>
2022-05-21 00:11:10 +0900

    dd

--- bellman_ford.py
+++ bellman_ford.py
@@ -12,27 +12,26 @@ def bf(start):
             cur = edges[j][0]
             next_node = edges[j][1]
             cost = edges[j][2]
-            if dist[cur] !=INF and dist[next_node]<dist[cur] + cost:
+            if dist[cur] !=INF and dist[next_node] > dist[cur] + cost:
                 dist[next_node] = dist[cur] + cost
                 if i == n-1:
                     return True
     return False
 
-n,m = map(int,input().split())
-
+n, m = map(int, input().split())
 edges = []
-dist = [INF] * (n+1)
+dist = [INF] * (n + 1)
 
 for _ in range(m):
     a, b, c = map(int, input().split())
-    edges.append((a,b,c))
+    edges.append((a, b, c))
 
 negative_cycle = bf(1)
 
 if negative_cycle:
     print(-1)
 else:
-    for i in range(2,n+1):
+    for i in range(2, n + 1):
         if dist[i] == INF:
             print(-1)
         else:
